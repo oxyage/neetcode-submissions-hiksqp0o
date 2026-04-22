797dc7af850df9144e4e111f8854c0a4347d5a72
oxyage <oxyage@yandex.ru>
2026-04-22 16:20:10 +0300

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.go
@@ -0,0 +1,11 @@
+func longestCommonPrefix(strs []string) string {
+	// todo:
+    for i := 0; i < len(strs[0]); i++ {
+        for _, s := range strs {
+            if i == len(s) || s[i] != strs[0][i] {
+                return s[:i]
+            }
+        }
+    }
+    return strs[0]
+}
