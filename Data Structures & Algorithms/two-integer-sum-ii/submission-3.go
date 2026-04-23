59e883ed9812cff2a6dd30b2700407fea9004605
oxyage <oxyage@yandex.ru>
2026-04-23 21:29:41 +0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.go
@@ -0,0 +1,25 @@
+func twoSum(numbers []int, target int) []int {
+
+	l, r := 0, len(numbers) - 1
+	result := make([]int, 0, 2)
+	for l < r {
+		// поскольку слайс отсортирован, движение левого указателя - точная подстройка
+		// движение правого указателя влево - грубая подстройка
+
+		// пусть res - промежуточный результат, res = numbers[l] + numbers[r]
+		res := numbers[l] + numbers[r]
+		if res == target{
+			return []int{l + 1, r + 1}
+		}
+		if res > target {
+		// тогда, если res > target, то нужна грубая подстройка, двигаем правый указатель влево
+			r--
+		} else {
+		// иначе двигаем левый указатель вправо
+			l ++
+		}
+	}
+
+
+	return result
+}
